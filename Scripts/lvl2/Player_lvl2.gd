13a1fedf4ce2066769829c516a7e7a817132acda
Salipoke <101245660+Salipoke@users.noreply.github.com>
2022-03-28 12:24:43 +0200

    lvl 2 començat
    Falta canviar totes les arees per static bodies. pq sóc tonto. Arreglat el bug del lvl 1.

--- Scripts/lvl2/Player_lvl2.gd
+++ Scripts/lvl2/Player_lvl2.gd
@@ -4,15 +4,13 @@ extends KinematicBody2D
 
 var base_vel = 250
 var vel = Vector2(0,0)
-var door_slide1 = 'False'
-export var inertia = 50
 var scene = 0
 var dir = Vector2(0,0)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = Vector2(512,300)
+	position = Vector2(600,300)
 	
 func _physics_process(delta):
 	vel = Vector2(0,0)
@@ -28,20 +26,5 @@ func _physics_process(delta):
 	if Input.is_action_pressed("down"):
 		vel += Vector2.DOWN * base_vel
 		dir[1] = 0
-	if door_slide1 == 'True':
-		pass
+	print(position)
 	vel = move_and_collide(vel*delta)
-
-func _on_Area2D_body_entered(body):
-	print ('a')
-	get_tree().change_scene("res://Scenes/Lvl 1/Gen.tscn")
-	
-func _on_Area2D2_body_entered(body):
-	print ('a')
-	get_tree().change_scene("res://Scenes/Lvl 2/lvl2.tscn")
-	
-func _on_Area2D3_body_entered(body):
-	print ('a')
-
-func _on_door_button_body_entered(body):
-	door_slide1 = 'True'
